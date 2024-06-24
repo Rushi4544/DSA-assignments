dc99e7cbf0bf143c6aba9c619eb49bdab443d343
Rushi4544 <rautrushikesh910@gmail.com>
2024-06-24 13:26:48 +0530

    problem1

---
+++ assignment1/assignment4/problem1.js
@@ -0,0 +1,27 @@
+
+// Given a 2D array (matrix), write a program to rotate the matrix by 90 degrees clockwise.
+
+function rotateMatrix90Clockwise(matrix) {
+    let n = matrix.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+    
+
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
+    }
+    
+    return matrix;
+}
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+let rotatedMatrix = rotateMatrix90Clockwise(matrix);
+console.log(rotatedMatrix);
